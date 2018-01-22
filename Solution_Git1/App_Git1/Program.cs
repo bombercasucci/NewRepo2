7e2275c9c534b4e0d2ed137ec51fcedca4bc798e
studenti <studenti@LAB2-PC-20>
2018-01-22 09:06:52 +0100

    si è effettuata una modifica al saluto!

---
+++ Solution_Git1/App_Git1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Git1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ciao");
+            Console.WriteLine("ho fatto una modifica!");
+            Console.ReadLine();
+        }
+    }
+}
